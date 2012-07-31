9f094a3090f281a888d4871938fcac46966c7ddc
Dan F <tazfreak294@hotmail.com>
2012-07-31 11:02:35 -0400

    Initial Commit

---
+++ TabBug2/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TabBug2
+//
+//  Created by Dan Fellmeth on 7/31/12.
+//  Copyright (c) 2012 Globus Medical Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TabBug2/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  TabBug2
+//
+//  Created by Dan Fellmeth on 7/31/12.
+//  Copyright (c) 2012 Globus Medical Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
